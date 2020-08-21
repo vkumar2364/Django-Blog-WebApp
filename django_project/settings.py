37c2da9d1dcc53e15fd3ca278c1ea204d0cc4236
vkumar2364 <63660635+vkumar2364@users.noreply.github.com>
2020-08-21 18:39:48 +0530

    1st commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from .secret_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,8 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+c%zodinw5&_+9&u7a9z!&s87wy%hyn5)9lzo+ccvy5@4z!0a2'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
